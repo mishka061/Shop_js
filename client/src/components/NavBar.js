5d93b143bf5304b33e041bf235543027a99ec27e
Алена Котова <mishka061@rambler.ru>
2023-01-17 14:22:16 +0300

    Поправка навбара

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,12 +6,15 @@ import {NavLink} from "react-router-dom";
 import {Button, Container} from "react-bootstrap";
 import {observer} from "mobx-react-lite";//для работы в реальном времени
 import {Context} from "../index";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
+import {useHistory} from "react-router-dom";
+
 
 //если пользователь авторизован,отображаем первый блок Если нет,то второй
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
+    const history = useHistory()
 
     return (
         <Navbar bg="dark" variant="dark">
@@ -19,8 +22,19 @@ const NavBar = observer(() => {
                 <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>КупиДевайс</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto" style={{color: "white"}}>
-                        <Button variant={"outline-light"}>Админ панель</Button>
-                        <Button variant={"outline-light"} className={"ml-2"}>Войти</Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history.push(ADMIN_ROUTE)}
+                        >
+                            Админ панель
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history.push(LOGIN_ROUTE)}
+                            className={"ml-2"}
+                        >
+                            Выйти
+                        </Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color: "white"}}>
