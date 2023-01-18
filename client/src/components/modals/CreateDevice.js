ed0e35fd1c81bfc717093932a541cf3601579a36
Алена Котова <mishka061@rambler.ru>
2023-01-18 12:41:32 +0300

    добавление кнопки выбора бренда

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import {Button, Form, Modal} from "react-bootstrap";
+import React, {useContext} from 'react';
+import {Button, Dropdown, Form, Modal} from "react-bootstrap";
+import {Context} from "../../index";
 
 const CreateDevice = ({show, onHide}) => {
+    const {device} = useContext(Context)
     return (
         <Modal
             show={show}
@@ -15,9 +17,23 @@ const CreateDevice = ({show, onHide}) => {
             </Modal.Header>
             <Modal.Body>
                 <Form>
-                    <Form.Control
-                        placeholder={"Введите название типа"}>
-                    </Form.Control>
+                    <Dropdown className="mt-2 mb-2">
+                        <Dropdown.Toggle>Выберите тип</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {device.types.map(type =>
+                            <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Dropdown className="mt-2 mb-2">
+                        <Dropdown.Toggle>Выберите бренд</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {device.brands.map(brand =>
+                                <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
+
                 </Form>
             </Modal.Body>
             <Modal.Footer>
