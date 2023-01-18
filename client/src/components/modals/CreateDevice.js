14dbadb4d4182e3e162f9588cc5506873029daab
Алена Котова <mishka061@rambler.ru>
2023-01-18 14:57:25 +0300

    функции добавления и удаления характеристик

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,9 +1,26 @@
-import React, {useContext} from 'react';
-import {Button, Dropdown, Form, Modal} from "react-bootstrap";
+import React, {useContext, useState} from 'react';
+import {Modal} from "react-bootstrap";
+import {Button, Dropdown, Form, Row, Col} from "react-bootstrap";
 import {Context} from "../../index";
 
 const CreateDevice = ({show, onHide}) => {
     const {device} = useContext(Context)
+    //характеристики для каждого устройства, пустой массив по умолчанию(info)
+    const [info, setInfo] = useState([])
+    //функция для добавления характеристик.
+    // Вызываем функцию которая изменяет состояние,в нее передаем массив,
+    // в этот массив передаем старый массив, и добавляем новый у которого есть тайтл,дескрипшион
+    const addInfo = () => {
+        setInfo([...info, {title: '', description: '', number: Date.now()}])
+
+
+    }
+    //для удаления характеристик
+    const removeInfo = (number) => {
+        setInfo(info.filter(i => i.number !== number))
+
+    }
+
     return (
         <Modal
             show={show}
@@ -21,7 +38,7 @@ const CreateDevice = ({show, onHide}) => {
                         <Dropdown.Toggle>Выберите тип</Dropdown.Toggle>
                         <Dropdown.Menu>
                             {device.types.map(type =>
-                            <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+                                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
                             )}
                         </Dropdown.Menu>
                     </Dropdown>
@@ -33,7 +50,46 @@ const CreateDevice = ({show, onHide}) => {
                             )}
                         </Dropdown.Menu>
                     </Dropdown>
-
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите название устройства"
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        placeholder="Введите стоимость устройства"
+                        type="number"
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        type="file"
+                    />
+                    <hr/>
+                    <Button variant={"outline-dark"}
+                            onClick={addInfo}
+                    >
+                        Добавить новое свойство
+                    </Button>
+                    {info.map(i =>
+                        <Row className="mt-4" key={i.number}>
+                            <Col md={4}>
+                                <Form.Control
+                                    placeholder="Введите название свойства"
+                                />
+                            </Col>
+                            <Col md={4}>
+                                <Form.Control
+                                    placeholder="Введите описание свойства"
+                                />
+                            </Col>
+                            <Col md={4}>
+                                <Button
+                                    onClick={() => removeInfo(i.number)}
+                                    variant={"outline-danger"}>
+                                    Удалить
+                                </Button>
+                            </Col>
+                        </Row>
+                    )}
                 </Form>
             </Modal.Body>
             <Modal.Footer>
