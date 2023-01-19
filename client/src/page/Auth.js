4c85e52e4edc5d6ff8e470dcfadaf97ccd65b713
Алена Котова <mishka061@rambler.ru>
2023-01-19 13:25:12 +0300

    подключение токена

--- client/src/page/Auth.js
+++ client/src/page/Auth.js
@@ -1,25 +1,35 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {Button, Container, Form} from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
-import {NavLink, useLocation} from "react-router-dom";
-import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
+import {NavLink, useHistory, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {login, registration} from "../http/userApi";
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
 
 
-const Auth = () => {
+const Auth = observer(() => {
+    const {user} = useContext(Context)
     const location = useLocation()
+    const history = useHistory()
     const isLogin = location.pathname === LOGIN_ROUTE
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const click = async () => {
-        if (isLogin) {
-            const response = await login();
-            console.log(response);
-        } else {
-            const response = await registration(email,password);
-            console.log(response);
+        try {
+            let data;
+            if (isLogin) {
+                 data = await login(email, password);
+            } else {
+                 data = await registration(email, password);
+            }
+            user.setUser(user)
+            user.setIsAuth(true)
+            history.push(SHOP_ROUTE)
+        }catch (e){
+            alert(e.response.data.message)
         }
     }
     return (
@@ -65,6 +75,6 @@ const Auth = () => {
             </Card>
         </Container>
     );
-};
+});
 
 export default Auth;
